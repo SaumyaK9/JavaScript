0d609aa26a862766522341b680b01111d66366a1
SaumyaK9 <135534345+SaumyaK9@users.noreply.github.com>
2024-03-28 14:23:16 +0000

    Operations

--- Basics/typeConversion.js
+++ Basics/typeConversion.js
@@ -21,8 +21,49 @@
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
-let x = 44
+// let x = 44
 
-let stringNumber = String(x)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// let stringNumber = String(x)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+// ***********************************Operations*********************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Saumya"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2);     //ecma script ToPrimitive()
+// console.log(1 + "2");     //jo datatype pehle aaega uska ho jaega type
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+//console.log(num1 , num2 , num3);
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+//mdn pe prefix postfix 
