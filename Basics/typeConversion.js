59f7d2c33ae1ab037cf0d7348cbd1a5f0109c3e5
SaumyaK9 <135534345+SaumyaK9@users.noreply.github.com>
2024-03-28 14:06:27 +0000

    Conversion of datatypes

---
+++ Basics/typeConversion.js
@@ -0,0 +1,28 @@
+// //let score = "33"      //---> Number
+// //let score = "33abc"   //---> NaN (Not a number)
+// //let score = null      //---> 0 
+// //let score = undefined //---> NaN
+// //let score = true      //---> 1
+// //let score = false     //---> 0
+// let score = "Saumya"    //---> NaN
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn = 1    // 1 ---> true , 0 ---> false
+// let isLoggedIn = ""   // "" ---> false
+// let isLoggedIn = "Saumya" // "Anystring" ---> true
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let x = 44
+
+let stringNumber = String(x)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
